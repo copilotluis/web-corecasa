368dbaf8f20c6695a481506c254654a8f7581efd
copilotluis <luisalbalat6@gmail.com>
2025-07-20 05:04:30 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/web-corecasa/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
