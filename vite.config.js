16df6699d333a14a155399baa547a80ed56437a6
Luis Alberto <luisalbalat1@gmail.com>
2025-07-20 04:20:42 -0400

    Descripción de los cambios realizados

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/WebCorecasa/',
+  base: '/web-corecasa/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: './' // Importante para GitHub Pages
 })
